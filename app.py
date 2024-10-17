310eeefed96699814d113f07d4d4bed45e38fabd
Freya <froyahaglund@gmail.com>
2024-10-17 14:06:34 +0200

    endre car identifier til kun car_id, lagt til customer funksjoner

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from neo4j import GraphDatabase
 
 
+# SYSTEM -----------------------------------------------------------------------------------------------------
 app = Flask(__name__) #instansiere flask applikasjon
 
 uri = "bolt://localhost:7687" #setter localhost adresse og kobler til Neo4j
@@ -36,19 +37,25 @@ def check_neo4j():
 def add_car():
 
     data = request.get_json() # behandle data fra post request, gjør om til dict
+    car_id = data['car_id']
     make = data['make'] # sette variabler for data fra post, til python
     model = data['model']
     year = data['year']
     location = data['location']
-    availability = data['status']
+    availability = data['availability']
+
+    exists = find_car(car_id)
+
+    if exists:
+        return jsonify({'message': 'denne finnes allerede, tulling :l'}), 200
 
     with driver.session() as session: # starte session med neo4j
         # lage ny eller merge node med data fra dict
         # ON CREATE = dersom ny lages, legger til c. til laget node
         session.run("""
-            MERGE (c:Car {make: $make, model: $model, year: $year})
-            ON CREATE SET c.location = $location, c.availability = $availability
-            """, make=make, model=model, year=year, location=location, availability=availability)
+            MERGE (c:Car {car_id: $car_id})
+            ON CREATE SET c.location = $location, c.availability = $availability, c.make = $make, c.model = $model, c.year = $year
+            """,car_id=car_id, make=make, model=model, year=year, location=location, availability=availability)
 
     return jsonify({"message": "Car added successfully!"}), 201
 
@@ -65,6 +72,7 @@ def retrieve_car():
             car_node = record['c'] # find the next node for car
 
             car_data = { # extract all data from car node
+                'car_id': car_node['car_id'],
                 'make': car_node['make'],
                 'model': car_node['model'],
                 'year': car_node['year'],
@@ -76,19 +84,64 @@ def retrieve_car():
 
     return jsonify(cars) # return jsonified data to client
 
+@app.route('/cars', methods=['PATCH']) # update car info
+def update_car():
 
-@app.route('/cars', methods=['PATCH'])
-def update_car(value,update):
-    pass
-
-# helper function
-def find_car(make, model, year):
+    data = request.get_json()
+    car_id = data['car_id']
+    make = data['make']
+    model = data['model']
+    year = data['year']
+    location = data['location']
+    availability = data['availability']
 
     with driver.session() as session:
         result = session.run("""
-                MATCH (c:Car {make: $make, model: $model, year: $year})
+            MATCH (c:Car {car_id: $car_id})
+            RETURN c
+        """, car_id=car_id)
+
+    car = result.single()
+
+    if car:
+        session.run("""
+            MATCH (c:Car {car_id: $car_id})
+            SET c.location = $location, c.availability = $availability, c.make = $make, c.model = $model, c.year = $year
+        """, car_id=car_id, make=make, model=model, year=year, availability=availability, location=location)
+
+        return jsonify({'message':'car new:)'}), 200
+
+    else:
+        return jsonify({'message':'no car!!!!!:o'}), 404
+
+@app.route('/cars', methods=['DELETE']) # delete car instance
+def remove_car():
+
+    data = request.get_json()
+    car_id = data['car_id']
+
+    exists = find_car(car_id)
+
+    if exists:
+        with driver.session() as session:
+            session.run("""
+                MATCH (c:Car {car_id: $car_id})
+                DELETE c
+                """, car_id=car_id)
+
+        return jsonify({"message": "Car removed successfully!"}), 204
+
+    else:
+        return jsonify({'message':'no car bby:('}), 404
+
+# helper function
+def find_car(car_id): #find specific car
+
+    with driver.session() as session:
+        result = session.run("""
+                MATCH (c:Car {car_id: $car_id})
                 RETURN c
-            """, make=make, model=model, year=year)
+            """, car_id=car_id)
 
         car = result.single()
 
@@ -97,6 +150,7 @@ def find_car(make, model, year):
             car_node = car['c']
 
             car_data = {  # extract all data from car node
+                'car_id': car_node['car_id'],
                 'make': car_node['make'],
                 'model': car_node['model'],
                 'year': car_node['year'],
@@ -104,16 +158,67 @@ def find_car(make, model, year):
                 'availability': car_node['availability']
                 }
 
-            return jsonify(car_data)
+            return car_data
 
         else:
-            return jsonify({'error':'car not found'}), 404
+            return None
 
 
 
 # CUSTOMERS --------------------------------------------------------------------------------------------------
 
+@app.route('/customer', methods=['POST']) # create customer instance
+def add_customer():
 
+    data = request.get_json()
+    customer_id = data['customer_id']
+    name = data['name']
+    age = data['age']
+    adress = data['adress']
+
+    exists = find_customer(customer_id)
+
+    if exists:
+        return jsonify({'message': 'denne finnes allerede, tulling :l'}), 200
+
+    with driver.session() as session:
+        session.run("""
+                MERGE (cus:Customer {customer_id: $customer_id})
+                ON CREATE SET c.name = $name, c.age = $age, c.adress = $adress
+                """, customer_id=customer_id, name=name, age=age, adress=adress)
+
+    return jsonify({"message": "Customer added successfully!"}), 201
+
+
+
+def find_customer(customer_id): #find specific customer
+
+    with driver.session() as session:
+        result = session.run("""
+                MATCH (cus:Customer {customer_id: $customer_id})
+                RETURN cus
+            """, customer_id=customer_id)
+
+        customer = result.single()
+
+        if customer:
+
+            customer_node = customer['cus']
+
+            customer_data = {  # extract all data from car node
+                'customer_id': customer_node['customer_id'],
+                'name': customer_node['name'],
+                'age': customer_node['age'],
+                'adress': customer_node['adress']
+                }
+
+            return customer_data
+
+        else:
+            return None
+
+
+# SYSTEM -----------------------------------------------------------------------------------------------------
 # ensure Neo4j driver is closed on app shutdown
 @app.teardown_appcontext
 def shutdown_session(exception=None):
